e44a16cabb5066280cb15e09e04e1f5d18e0fb41
9yutae <9yutae@gmail.com>
2025-03-16 22:06:40 +0900

    Initial commit

---
+++ Source/BullsAndCows/BACLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BACLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLSANDCOWS_API UBACLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
